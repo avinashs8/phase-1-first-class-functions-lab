d17bdbd14068901f141489bc2b52144f796f8f08
avinash singh <avinash8.as@gmail.com>
2022-11-27 22:25:37 -0500

    commit tree 807289326e9d9d3f350b669671f1f590c8ca2efe

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const names = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers (parameter){
+    return parameter.slice(0,2)
+}
+
+function returnLastTwoDrivers (parameter){
+    return parameter.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return function (price){
+        return multiplier * price
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (names, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(names)
+}
